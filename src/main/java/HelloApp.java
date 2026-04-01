46a72e5595aebe15248c4e68090717c10b2d6012
Dhruv k <dk8159@srmist.edu.in>
2026-04-02 03:10:27 +0530

    Fix minor bug in uc2

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -7,6 +7,6 @@ public class HelloApp
     public static void main( String[] args )
     {
         String name = args[0];
-        System.out.println("Hello " + args[0] + "!");
+        System.out.println("Hello " + name + "!");
     }
 }
