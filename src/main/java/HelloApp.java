fe81a61e12ffdf6c37290c4e160e9f4e13decaaa
Dhruv k <dk8159@srmist.edu.in>
2026-04-02 03:05:18 +0530

    UC2

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -6,6 +6,7 @@ public class HelloApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String name = args[0];
+        System.out.println("Hello " + args[0] + "!");
     }
 }
